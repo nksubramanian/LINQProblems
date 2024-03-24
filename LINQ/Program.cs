4f11a78e706e67c4b1deebd8dd60ac619ad09424
subramanian <subramanian.kumarappan@halliburton.com>
2024-03-24 19:50:21 +0530

    RUNNING SUM ACHIEVED

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
-            List<int> doublednuumbers = numbers.Select(x=>2*x).ToList();
-            int g = 8;
+            List<int> runningSum = numbers.Select((number, index)=> numbers.Take(index+1).Sum()).ToList();
+            runningSum.ForEach(number => Console.WriteLine(number));    
 
 
 
