8a7eb94ce923c44b63b7e5f1d5e473103952b3f8
subramanian <subramanian.kumarappan@halliburton.com>
2024-03-24 21:45:43 +0530

    running sum

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,13 +1,19 @@
-﻿namespace LINQ
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
-            List<int> runningSum = numbers.Select((number, index)=> numbers.Take(index+1).Sum()).ToList();
-            runningSum.ForEach(number => Console.WriteLine(number));    
-
+            List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            int res = numbers.Aggregate((sum, val)=>sum+val);
+            var runningSum = numbers.Aggregate(new List<int>(), (acc, val) =>
+            {
+                acc.Add(val+acc.LastOrDefault());
+                return acc;
+            }).ToList();
 
 
 
