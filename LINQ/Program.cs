64ea28784e80a6658c9e264715929e0ef1eb95eb
subramanian <subramanian.kumarappan@halliburton.com>
2024-03-24 19:43:17 +0530

    select numbers done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            List<int> doublednuumbers = numbers.Select(x=>2*x).ToList();
+            int g = 8;
+
+
+
+
+
+
         }
     }
 }
