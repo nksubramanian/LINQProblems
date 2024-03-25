92c82d14f89b2d1a102e431e5b2b2fd65306a16c
subramanian <subramanian.kumarappan@halliburton.com>
2024-03-25 12:05:06 +0530

    temporary commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -13,11 +13,14 @@ namespace LINQ
         {
             List<int> a = new List<int> { 1, 2, 3, 4, 5, 6, 7};
             List<int> b = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
-            IEnumerable<int> f = a.Add(b);
+            List<int> c = new List<int> { };
+            IEnumerable<int> f = a.Add(c);
             var g =f.ToList();
             var h = Enumerable.Repeat(0, 10);
 
             List<int> answer = 4.Multiply(a).ToList();
+
+            
 
 
 
